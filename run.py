53ecca659a10fadfe93c8a79b8e5bb5486ef4179
Jibril <jibrilhp@outlook.com>
2019-12-30 21:46:51 +0700

    okedah

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import urllib.request
 from bs4 import BeautifulSoup
 import time
 import requests
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def make_soup(url):
     thepage = urllib.request.urlopen(url)
@@ -10,41 +13,50 @@ def make_soup(url):
     return soupdata
 
 def save_to_server(list_data):
-    print(list_data)
-    params_data = {'id_masjid':list_data[4],
-        'kabupaten_kota':list_data[1],
-        'kecamatan':list_data[2],
-        'nama_masjid':list_data[3],
-        'tipologi':list_data[5],
-        'alamat':list_data[6],
-        'latitude':0,
-        'longitude':0,
-        'luas_tanah':list_data[7],
-        'status_tanah':list_data[8],
-        'luas_bangunan':list_data[9],
-        'tahun_berdiri':list_data[10],
-        'jumlah_jamaah':list_data[11],
-        'jumlah_imam':list_data[12],
-        'jumlah_khatib':list_data[13],
-        'jumlah_muazin':list_data[14],
-        'jumlah_remaja':list_data[15],
-        'nomor_kontak':list_data[16],
-        'source_data':"SIMAS" }
-    r = requests.post("https://jibs.my.id/api/masjid/saveMasjid",data=params_data)
+    if len(list_data) > 1:
+        params_data = {'id_masjid':list_data[4],
+            'kabupaten_kota':list_data[1],
+            'kecamatan':list_data[2],
+            'nama_masjid':list_data[3],
+            'tipologi':list_data[5],
+            'alamat':list_data[6],
+            'latitude':0,
+            'longitude':0,  
+            'luas_tanah':list_data[7],
+            'status_tanah':list_data[8],
+            'luas_bangunan':list_data[9],
+            'tahun_berdiri':list_data[10],
+            'jumlah_jamaah':list_data[11],
+            'jumlah_imam':list_data[12],
+            'jumlah_khatib':list_data[13],
+            'jumlah_muazin':list_data[14],
+            'jumlah_remaja':list_data[15],
+            'nomor_kontak':list_data[16],
+            'source_data':"SIMAS" }
+    else:
+        params_data = None
+    if (params_data != None):
+        r = requests.post("https://jibs.my.id/api/masjid/saveMasjid",data=params_data)
+    else:
+        return "None"
+
     return r.text
 
-for x in range(0,41860,10):
+for x in range(0,0,10):
     
     if (x == 0):
-        soup = make_soup('http://simas.kemenag.go.id/index.php/profil/masjid/?tipologi_id=7')
+        
+        soup = make_soup('https://simas.kemenag.go.id/index.php/profil/masjid/?tipologi_id=1')
     else:
-        soup = make_soup('http://simas.kemenag.go.id/index.php/profil/masjid/page/' + str(x) + '?tipologi_id=7')
+        soup = make_soup('https://simas.kemenag.go.id/index.php/profil/masjid/page/' + str(x) + '?tipologi_id=2')
 
     for record in soup.findAll('tbody'):
         for data in record.findAll('tr'):
             masjid_data = []
             for data2 in data.findAll('td'):
                 masjid_data.append(data2.text)
-            print(save_to_server(masjid_data))
+            sr = save_to_server(masjid_data)
+            if "400" in sr:
+                print(sr,str(x))
     
     
