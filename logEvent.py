650dba3d6d8217bef8a709bd5728f265cb07a11d
Jibril Hartri <jibrilhp@student.gunadarma.ac.id>
2020-04-07 19:09:20 +0700

    Update logEvent.py

--- logEvent.py
+++ logEvent.py
@@ -5,4 +5,4 @@ import urllib
 def sendLogMessage(message):
     pool = Pool(1)
     parsedMessage = urllib.parse.quote(message)
-    pool.apply_async(requests.get, ['https://api.telegram.org/bot1049146609:AAF986C7CUE4fZTKy8FYKSylzKyEFakVBLg/sendMessage?chat_id=67793950&text=' + str(parsedMessage)])
+    pool.apply_async(requests.get, ['https://api.telegram.org/<ss>/sendMessage?chat_id=67793950&text=' + str(parsedMessage)])
